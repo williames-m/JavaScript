d69364fb1b37688cc417082ea68f8091aafb5731
williames-m <96849274+williames-m@users.noreply.github.com>
2021-12-29 20:56:28 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,27 @@
+//codigo das imagens e sons do jogo
+
+//imagens
+let imagemDaEstrada;
+let ImagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+  
+  somDaTrilha = loadSound("som/trilha.mp3");
+  somDaColisao = loadSound("som/colidiu.mp3");
+  somDoPonto = loadSound("som/pontos.wav");
+}
+
